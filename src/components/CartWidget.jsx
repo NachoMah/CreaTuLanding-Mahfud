2fca1ecf04040c92f34c7260115bae244ee83031
NachoMah <ignaciomahfud@gmail.com>
2025-12-21 16:20:11 -0300

    Primero commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import { RiShoppingCart2Line } from 'react-icons/ri'
+
+const CartWidget = () => {
+    return(
+        <div>
+            <RiShoppingCart2Line/>
+        </div>
+    )
+
+}
+
+export default CartWidget;
