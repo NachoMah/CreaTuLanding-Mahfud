94ce3e56e4c9282a13c80394394092c7b75c5d0d
NachoMah <ignaciomahfud@gmail.com>
2025-12-21 20:08:51 -0300

    Se agrega un  badge

--- src/components/CartWidgetRI.jsx
+++ src/components/CartWidgetRI.jsx
@@ -1,9 +1,11 @@
 import { RiShoppingCart2Line } from 'react-icons/ri'
+import Badge from 'react-bootstrap/Badge';
 
 const CartWidgetRI = () => {
     return(
         <div className="cart-widget-item">
             <RiShoppingCart2Line/>
+            <Badge className="cart-badge" bg="warning" text="dark"> 4 </Badge>
         </div>
     );
 
