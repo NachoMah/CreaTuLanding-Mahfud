5a638335ed7f6949dabb89626cc3d3521d4f11cf
NachoMah <ignaciomahfud@gmail.com>
2026-01-24 15:37:28 -0300

    Se instalan e importan la libreria de react-router y se encuelven componentes en el BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBarBS from "./components/NavBarBS";
 import ItemListContainer from "./components/ItemListContainer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemDetailContainer from './components/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -11,15 +12,20 @@ function App() {
   const fuerza = "¡Que la Fuerza te acompañe!"
 
   return (
-    <>
+    <BrowserRouter>
       <NavBarBS/>
-      <ItemListContainer 
-        bienvenida={bienvenida}
-        introduccion={introduccion}
-        fuerza={fuerza}
-      />
-      <ItemDetailContainer/>
-    </>
+
+      <Routes>
+        <Route path="/" element = {<ItemListContainer 
+          bienvenida={bienvenida}
+          introduccion={introduccion}
+          fuerza={fuerza}/>}  
+        />
+
+        <Route path="/item" element={<ItemDetailContainer/>} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
