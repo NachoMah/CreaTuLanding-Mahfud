19ace753e155645adcbae111ee837e07e938ff9d
NachoMah <ignaciomahfud@gmail.com>
2026-02-14 16:53:12 -0300

    Se instala fireabse, y se crea una carpeta junto a un archivo que contiene el sdk

---
+++ src/service/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnGENKNM0LW7wvca85p03GDl4v2nowJyk",
+  authDomain: "proyecto-react-coderhous-c5f1a.firebaseapp.com",
+  projectId: "proyecto-react-coderhous-c5f1a",
+  storageBucket: "proyecto-react-coderhous-c5f1a.firebasestorage.app",
+  messagingSenderId: "952310225184",
+  appId: "1:952310225184:web:766699c9ac674afad21f45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
